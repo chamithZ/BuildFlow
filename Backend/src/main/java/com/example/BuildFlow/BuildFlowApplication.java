284403f6306d95182fd36ba1a18dabdb90c31c0a
it21156960 <chamith227@gmail.com>
2023-10-03 15:51:31 +0530

    initial commit

---
+++ Backend/src/main/java/com/example/BuildFlow/BuildFlowApplication.java
@@ -0,0 +1,15 @@
+package com.example.BuildFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class BuildFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildFlowApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/BuildFlow/BuildFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BuildFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
